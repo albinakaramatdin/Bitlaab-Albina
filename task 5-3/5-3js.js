d3698c344b01f1fcdcd76e43d6078307384dcfc9
Albina Karamatdin <75879092+albinakaramatdin@users.noreply.github.com>
2024-01-18 19:44:36 +0600

    Add files via upload

---
+++ task 5-3/5-3js.js
@@ -0,0 +1,9 @@
+function getUserInfo() {
+
+            var name = prompt("Enter your name:", "");
+            var surname = prompt("Enter your last name:", "");
+            var city = prompt("Enter your city:", "");
+            var age = prompt("Enter your age:", "");
+
+            alert("Name: " + name + "\nSurname: " + surname + "\nCity: " + city + "\nAge: " + age);
+        }
